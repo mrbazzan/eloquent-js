fcbd75f29969298a1ca1c3b7bba046622f95bd79
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-04-15 10:42:08 +0100

    chapter 2: dispatch and loops

---
+++ loops.js
@@ -0,0 +1,19 @@
+
+// while loop
+let number = 0
+while (number <= 12){
+    console.log(number);
+    number = number + 2
+}
+
+// do-while loop
+let the_name;
+do{
+    the_name = prompt("Who are you? ");
+} while (!the_name);
+console.log("Hello, " + the_name)
+
+// for loop
+for (let number = 0; number <= 12; number = number + 2){
+    console.log(number);
+}
