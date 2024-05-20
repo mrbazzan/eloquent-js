e2e99bc37390ecc84538a44f37e5511f54e678fb
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-05-20 11:13:01 +0100

    functions: bindings and scope

---
+++ function.js
@@ -0,0 +1,30 @@
+
+const square = function(x){ return x*x; }
+
+const roundTo = function(n, step){
+    let remainder = n - step;
+    return n - remainder + (remainder < step / 2 ? 0 : step);
+}
+
+const hummus = function(factor) {
+    const ingredient = function(amount, unit, name) {
+        let ingredientAmount = amount * factor;
+        if (ingredientAmount > 1) {
+            unit += "s";
+        }
+        console.log(`${ingredientAmount} ${unit} ${name}`);
+    };
+    ingredient(1, "can", "chickpeas");
+    ingredient(0.25, "cup", "tahini");
+    ingredient(0.25, "cup", "lemon juice");
+    ingredient(1, "clove", "garlic");
+    ingredient(2, "tablespoon", "olive oil");
+    ingredient(0.5, "teaspoon", "cumin");
+ };
+
+ function cube(x){
+    return x*x*x;
+ }
+
+ const quad = x => x*x*x*x;
+ 
