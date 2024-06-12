d3d50e15ec89579761dce0e79a80aa03d4b46c3b
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-06-12 18:30:58 +0100

    update exercise 4 part 1

--- exercise/4.js
+++ exercise/4.js
@@ -1,17 +1,19 @@
 // SUM OF A RANGE
-function range(start=0, end, step=1){
-    let arr = [];
-    while (start <= end){
-        arr.push(start);
-        start += step;
-    }
-    return arr;``
-}
-
-function sum(arr){
+function sum(arr) {
     let total = 0;
-    for (let num of arr){
+    for (let num of arr) {
         total += num;
     }
     return total;
+}
+
+function range(start, end, step=1) {
+    let arr = [], state = true;
+    while (step && state){
+        arr.push(start);
+        start += step;
+        if (step < 0) state = (start >= end);
+        else state = (start <= end);
+    }
+    return arr;
 }
