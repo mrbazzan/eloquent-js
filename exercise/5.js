dcb83c6dae3f9699585c5a86b19cd194f92ebd78
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-07-29 09:52:13 +0100

    chapter 5: exercise contd.

--- exercise/5.js
+++ exercise/5.js
@@ -19,3 +19,38 @@ function loop(value, test, update, body){
 }
 
 loop(0, x=>x<=5, x=>x=x+1, x=>console.log(x));
+
+// EVERYTHING
+
+function everyUsingLoop(arr, predicate){
+    for (let element of arr){
+        if (!predicate(element)) return false;
+    }
+    return true;
+}
+console.log(everyUsingLoop([1,2,3], x=>x>1));
+
+function everyUsingSome(arr, predicate){
+    // `some` returns false unless callBackFn returns
+    // truthy value, in which case, the true is immediately
+    // returned.
+    return !arr.some((num) => !predicate(num));
+}
+console.log(everyUsingSome([1,2,3], x=>x>1));
+
+// DOMINANT WRITING DIRECTION
+// The dominant direction is the direction of a
+// majority of the characters that have a script
+// associated with them.
+function dominantWritingDirection(text){
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script? script.direction: "none";
+    }).filter(({name}) => name != "none");
+
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found."
+
+    // Find the most dominant based on count.
+    return scripts.reduce((a, b) => a.count > b.count? a: b).name;
+}
